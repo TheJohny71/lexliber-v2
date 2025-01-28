f9b40bb245ef32fbc6f8dbdf6d12c5077eba6301
TheJohny71 <jduval71@me.com>
2025-01-28 20:04:37 +0000

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="min-h-screen bg-gradient-to-b from-gray-50/50 to-white dark:from-gray-900 dark:to-gray-800">
-            {children}
-          </div>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
         </ThemeProvider>
       </body>
     </html>
