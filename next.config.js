21796c7b770424bc5b521c404748c45950bcb43d
TheJohny71 <jduval71@me.com>
2025-01-29 22:42:54 +0000

    updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js configuration options go here
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000"]
+    }
+  },
+  typescript: {
+    ignoreBuildErrors: false
+  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
